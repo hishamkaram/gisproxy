3b8b234db28e3e61ec45788e55e676a230dd0352
hisham waleed karam <hisham.karam@cartologic.com>
2018-09-17 09:02:34 +0200

    add ReadFile

--- utils.go
+++ utils.go
@@ -1,6 +1,8 @@
 package gisproxy
 
 import (
+	"io/ioutil"
+
 	"github.com/sirupsen/logrus"
 )
 
@@ -11,5 +13,16 @@ func GetLogger() (logger *logrus.Logger) {
 	Formatter.TimestampFormat = "02-01-2006 15:04:05"
 	Formatter.FullTimestamp = true
 	logger.Formatter = Formatter
+	return
+}
+
+//ReadFile read file
+func ReadFile(path string) (data []byte, err error) {
+	logger := GetLogger()
+	data, err = ioutil.ReadFile(path)
+	if err != nil {
+		logger.Error(err)
+		return
+	}
 	return
 }
