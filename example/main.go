a7c7b114cd311859e6ee4eaf3f7e88ea27d5fea1
hisham waleed karam <hisham.karam@cartologic.com>
2018-09-17 07:49:23 +0200

    remove comments from example

--- example/main.go
+++ example/main.go
@@ -4,19 +4,7 @@ import (
 	"github.com/hishamkaram/gisproxy"
 )
 
-// func geoserverHandler(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
-// 	return func(w http.ResponseWriter, r *http.Request) {
-// 		log.Println(r.URL)
-// 		r.SetBasicAuth("admin", "geoserver")
-// 		defer r.Body.Close()
-// 		p.ServeHTTP(w, r)
-// 	}
-// }
 func main() {
-	// remote, err := url.Parse("http://localhost:8080/geoserver")
-	// if err != nil {
-	// 	panic(err)
-	// }
 	server := gisproxy.GISProxy{
 		DB:      &gisproxy.DBConnection{Name: "gisproxy", Password: "clogic", Username: "hishamkaram", Port: 5432, Host: "localhost"},
 		Address: ":8081",
